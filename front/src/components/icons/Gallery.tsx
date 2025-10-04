fbe685bd453cda22afad8ed3d04054914ef94476
Diego Ricardo <diegoricardoweb@gmail.com>
2025-10-04 16:56:06 -0300

    feat: add component Gallery

---
+++ front/src/components/icons/Gallery.tsx
@@ -0,0 +1,7 @@
+export function Gallery() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 17 17">
+      <path id="Caminho_131" data-name="Caminho 131" d="M17.3,3.7V13.9H7.1V3.7H17.3m0-1.7H7.1A1.7,1.7,0,0,0,5.4,3.7V13.9a1.7,1.7,0,0,0,1.7,1.7H17.3A1.7,1.7,0,0,0,19,13.9V3.7A1.7,1.7,0,0,0,17.3,2Zm-7.225,8.219,1.437,1.921,2.108-2.635,2.83,3.544H7.95ZM2,5.4V17.3A1.7,1.7,0,0,0,3.7,19H15.6V17.3H3.7V5.4Z" transform="translate(-2 -2)" fill="#fff" />
+    </svg>
+  )
+}
