9d87f787e91eda9c5e156c7c587234a629578927
Diego Ricardo <diegoricardoweb@gmail.com>
2025-10-04 16:56:21 -0300

    feat: add component parkingSpace

---
+++ front/src/components/icons/ParkingSpace.tsx
@@ -0,0 +1,14 @@
+export function ParkingSpace() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30">
+      <g id="Grupo_22394" data-name="Grupo 22394" transform="translate(-942 -977)">
+        <g id="Grupo_22158" data-name="Grupo 22158" transform="translate(946 982)">
+          <path id="Caminho_139" data-name="Caminho 139" d="M22.9,6.263A1.868,1.868,0,0,0,21.125,5H7.375A1.88,1.88,0,0,0,5.6,6.263L3,13.75v10A1.254,1.254,0,0,0,4.25,25H5.5a1.254,1.254,0,0,0,1.25-1.25V22.5h15v1.25A1.254,1.254,0,0,0,23,25h1.25a1.254,1.254,0,0,0,1.25-1.25v-10ZM7.812,7.5H20.675l1.35,3.887H6.462ZM23,20H5.5V13.75H23Z" transform="translate(-3 -5)" fill="#c5af62" />
+          <circle id="Elipse_48" data-name="Elipse 48" cx="1.825" cy="1.825" r="1.825" transform="translate(3.914 10.094)" fill="#c5af62" />
+          <ellipse id="Elipse_49" data-name="Elipse 49" cx="2.027" cy="1.825" rx="2.027" ry="1.825" transform="translate(14.861 10.094)" fill="#c5af62" />
+        </g>
+        <rect id="Retângulo_3379" data-name="Retângulo 3379" width="30" height="30" transform="translate(942 977)" fill="none" />
+      </g>
+    </svg>
+  )
+}
