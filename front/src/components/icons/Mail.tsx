202161f4704a047d6a5c401e56493f1e1be2a848
Diego Ricardo <diegoricardoweb@gmail.com>
2025-10-04 16:56:34 -0300

    feat: add component Mail

---
+++ front/src/components/icons/Mail.tsx
@@ -0,0 +1,17 @@
+
+interface IconProps {
+  classname?: string;
+}
+
+export function Mail({ classname }: IconProps) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25" className={classname}>
+      <g id="Grupo_22378" data-name="Grupo 22378" transform="translate(-1.872 -0.21)">
+        <g id="Grupo_700" data-name="Grupo 700" transform="translate(1.872 0.21)">
+          <rect id="Retângulo_371" data-name="Retângulo 371" width="25" height="25" fill="none" />
+        </g>
+        <path id="Caminho_1289" data-name="Caminho 1289" d="M27,6.5A2.507,2.507,0,0,0,24.5,4H4.5A2.507,2.507,0,0,0,2,6.5v15A2.507,2.507,0,0,0,4.5,24h20A2.507,2.507,0,0,0,27,21.5Zm-2.5,0-10,6.25L4.5,6.5Zm0,15H4.5V9l10,6.25L24.5,9Z" transform="translate(-0.128 -0.79)" fill="currentColor" />
+      </g>
+    </svg>
+  )
+}
