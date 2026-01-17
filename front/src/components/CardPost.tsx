36b5647116012ed8c0b0b3b262eb6646265d5cdf
Diego Ricardo <diegoricardoweb@gmail.com>
2026-01-17 20:14:01 -0300

    fix: fix image

--- front/src/components/CardPost.tsx
+++ front/src/components/CardPost.tsx
@@ -1,29 +1,33 @@
+/* eslint-disable @next/next/no-img-element */
 import { Post } from "@/entities/Post";
 import { trimWords } from "@/lib/trimWords";
 import { cn } from "@/lib/utils";
-import Image from "next/image";
 import Link from "next/link";
 import { LuArrowRight } from "react-icons/lu";
 
 interface CardPostProps {
-  post: Post
-  className?: string
-  type: 'with button' | 'simple'
+  post: Post;
+  className?: string;
+  type: "with button" | "simple";
 }
 
 export function CardPost({ post, className, type }: CardPostProps) {
-  const featuredImageUrl = post.medias?.filter((media) => media.mediaType === 'featured_image')[0]?.url;
+  const featuredImageUrl = post.medias?.filter(
+    (media) => media.mediaType === "featured_image",
+  )[0]?.url;
   const category = post.categories?.[0]?.name;
 
   return (
     <article>
-      <div className={cn(
-        'border-b border-gold',
-        type === 'with button' && 'pb-[30px]',
-        className
-      )}>
+      <div
+        className={cn(
+          "border-b border-gold",
+          type === "with button" && "pb-[30px]",
+          className,
+        )}
+      >
         <Link href={`/${post.slug!}`}>
-          <Image
+          <img
             src={`${process.env.NEXT_PUBLIC_API_URL}${featuredImageUrl!}`}
             width={376}
             height={282}
@@ -31,7 +35,9 @@ export function CardPost({ post, className, type }: CardPostProps) {
             className="rounded-[0_20px_20px_20px] h-[282px] w-full object-cover object-center"
           />
           <header className="mt-[9px]">
-            <span className="font-inter text-[14px] text-title font-medium uppercase">{category}</span>
+            <span className="font-inter text-[14px] text-title font-medium uppercase">
+              {category}
+            </span>
             <h2 className="font-montserrat text-gold text-[22px] leading-[26px] font-medium mb-[10px]">
               {post.name}
             </h2>
@@ -41,9 +47,12 @@ export function CardPost({ post, className, type }: CardPostProps) {
           </p>
         </Link>
 
-        {type === 'with button' && (
+        {type === "with button" && (
           <div className="flex justify-center mt-[15px]">
-            <Link href={`/${post.slug!}`} className="button-geral !text-white shadow-[0px_3px_6px_#00000029] !bg-black hover:!bg-gold">
+            <Link
+              href={`/${post.slug!}`}
+              className="button-geral !text-white shadow-[0px_3px_6px_#00000029] !bg-black hover:!bg-gold"
+            >
               CONTINUE LENDO
               <LuArrowRight className="w-[15px] h-[15px] lg:w-[18px] lg:h-[18px] ml-[-3px]" />
             </Link>
@@ -51,5 +60,5 @@ export function CardPost({ post, className, type }: CardPostProps) {
         )}
       </div>
     </article>
-  )
+  );
 }
