14b8202f3a2220b44d54adbf8376e78b01620592
Diego Ricardo <diegoricardoweb@gmail.com>
2026-01-19 21:26:46 -0300

    feat: change title site

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -9,7 +9,7 @@ import { LuArrowRight } from "react-icons/lu";
 import { getSettingsAction } from "./actions/settings/get-settings";
 
 // Adicione esta linha para tornar a página dinâmica
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 
 export async function generateMetadata(): Promise<Metadata> {
   try {
@@ -24,22 +24,24 @@ export async function generateMetadata(): Promise<Metadata> {
 
     return {
       title: `Úniko Imóveis`,
-      description: settings[0].descriptionSeo || "Úniko Imóveis - Melhores imóveis no Brasil",
+      description:
+        settings[0].descriptionSeo ||
+        "Úniko Imóveis - Melhores imóveis no Brasil",
       openGraph: {
         title: settings[0].titleSeo,
         description: settings[0].descriptionSeo,
-        images: '/logo.png',
-        type: 'article',
+        images: "/logo.png",
+        type: "article",
       },
       twitter: {
-        card: 'summary_large_image',
+        card: "summary_large_image",
         title: settings[0].titleSeo,
         description: settings[0].descriptionSeo,
-        images: '/logo.png',
+        images: "/logo.png",
       },
     };
   } catch (error) {
-    console.error('Erro ao gerar metadata:', error);
+    console.error("Erro ao gerar metadata:", error);
 
     return {
       title: "Erro - Úniko Imóveis",
@@ -52,9 +54,7 @@ export default function Home() {
   return (
     <div>
       <div className="relative h-screen">
-        <div
-          className="absolute top-0 left-0 w-full h-full bg-black/45 flex flex-col z-10"
-        >
+        <div className="absolute top-0 left-0 w-full h-full bg-black/45 flex flex-col z-10">
           <header className="w-full bg-black">
             <div className="container grid grid-cols-2 lg:grid-cols-3">
               <div className="hidden lg:block"></div>
@@ -81,11 +81,10 @@ export default function Home() {
           </header>
 
           <div className="flex flex-col items-center justify-center h-full flex-1">
-            <h1
-              className="text-white text-[40px] leading-[43px] lg:text-[48px] lg:leading-[58px] font-light font-montserrat text-center mb-10"
-            >
-              O IMÓVEL DOS SEUS SONHOS<br />
-              <strong className="font-semibold">É ESPECIAL E ÚNIKO</strong>
+            <h1 className="text-white text-[40px] leading-[43px] lg:text-[48px] lg:leading-[58px] font-light uppercase font-montserrat text-center mb-10">
+              <strong className="font-semibold">ÚNIKO IMÓVEIS</strong>
+              <br />
+              Exclusividade em cada metro quadrado
             </h1>
 
             <div className="flex justify-center gap-4 lg:gap-8">
@@ -93,14 +92,16 @@ export default function Home() {
                 href="/imoveis?city=curitiba"
                 className="flex items-center bg-white text-[#343434] px-[18px] py-[13px] lg:px-[20px] lg:py-[15px] font-montserrat text-[15px] leading-[19px] lg:text-[18px] lg:leading-[22px] font-medium gap-[3px] shadow-[0px_3px_6px_#00000029] transition-all hover:bg-[#C5AF62] hover:text-white mb-2.5 lg:mb-0 justify-center"
               >
-                Curitiba <LuArrowRight className="w-[15px] h-[15px] lg:w-[19px] lg:h-[19px]" />
+                Curitiba{" "}
+                <LuArrowRight className="w-[15px] h-[15px] lg:w-[19px] lg:h-[19px]" />
               </Link>
 
               <Link
                 href="/imoveis?city=belo-horizonte"
                 className="flex items-center bg-white text-[#343434] px-[18px] py-[13px] lg:px-[20px] lg:py-[15px] font-montserrat text-[15px] leading-[19px] lg:text-[18px] lg:leading-[22px] font-medium gap-[3px] shadow-[0px_3px_6px_#00000029] transition-all hover:bg-[#C5AF62] hover:text-white mb-2.5 lg:mb-0 justify-center"
               >
-                Belo Horizonte <LuArrowRight className="w-[15px] h-[15px] lg:w-[19px] lg:h-[19px]" />
+                Belo Horizonte{" "}
+                <LuArrowRight className="w-[15px] h-[15px] lg:w-[19px] lg:h-[19px]" />
               </Link>
             </div>
           </div>
